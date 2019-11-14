a0f633d6ed2666fd88c57328f0cc5c2d65ade1c8
eccentricnode <shival.bigman@gmail.com>
2019-11-13 17:09:48 -0700

    setup modal in ui-modal lib

--- client/libs/ui-modal/src/lib/ui-modal.module.ts
+++ client/libs/ui-modal/src/lib/ui-modal.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogRef } from '@angular/material/dialog'
+import { UiModalComponent } from './ui-modal/ui-modal.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule, MatDialogRef],
+  declarations: [UiModalComponent]
 })
 export class UiModalModule {}
