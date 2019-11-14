90a8b2a227730d168186588b345bd71f1faf1132
david <david@venturplex.com>
2019-11-14 06:22:11 -0700

    I have generated components to satisfy routes. I have updated modules to export components. Added the routing file and updated it.

--- client/libs/searchbar/src/lib/searchbar.module.ts
+++ client/libs/searchbar/src/lib/searchbar.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchbarComponent } from './searchbar.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [SearchbarComponent],
+  exports: [SearchbarComponent]
 })
 export class SearchbarModule {}
