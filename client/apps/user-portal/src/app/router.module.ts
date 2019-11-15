53788fd4648e4760eb7c147f1a161eb4146eaa84
eccentricnode <shival.bigman@gmail.com>
2019-11-15 11:54:05 -0700

    fixed spacing on router.module

--- client/apps/user-portal/src/app/router.module.ts
+++ client/apps/user-portal/src/app/router.module.ts
@@ -2,18 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SermonsComponent, SermonListComponent } from '@app/sermons';
 
-
 const routes: Routes = [
-    { 
-        path: '', component: SermonsComponent, children: [
-            { path: ':id', component: SermonListComponent }
-        ]
-    }
+  { 
+    path: '', component: SermonsComponent, children: [
+      { path: ':id', component: SermonListComponent }
+    ]
+  }
 ];
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes),
+      RouterModule.forRoot(routes),
     ],
     exports: [RouterModule],
     declarations: [],
