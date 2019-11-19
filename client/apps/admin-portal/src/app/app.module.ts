f05f851dd67b713228231a8b588f226da1c9ad30
david <david@venturplex.com>
2019-11-19 15:21:25 -0700

    adressed PR request

--- client/apps/admin-portal/src/app/app.module.ts
+++ client/apps/admin-portal/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ManageComponent } from './manage/manage.component';
 import { RoutingModule } from './routing.module';
-import { MaterialModule } from '@app/material';
-import { SermonsModule } from '@app/sermons';
-import { SearchbarModule } from '@app/searchbar';
+import { MaterialModule } from '@sb/material';
+import { SermonsModule } from '@sb/sermons';
+import { SearchbarModule } from '@sb/searchbar';
 
 
 @NgModule({
