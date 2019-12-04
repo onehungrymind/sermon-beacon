cce17691f496b1e3451394b80004c5039dd95d59
david <david@venturplex.com>
2019-12-04 14:27:19 -0700

    created service for sermons. Now to test it works

---
+++ client/libs/core-data/src/lib/sermons/sermons.graphql.ts
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag';
+
+export const sermonFragment = gql`
+  fragment sermonFragment on sermons {
+    id
+    title
+    subject
+    date
+    created_at
+    updated_at
+  }
+`;
+
+export const sermonQuery = gql`
+  query sermonQuery {
+    sermons {
+      ...sermonFragment
+    }
+  }
+  ${sermonFragment}
+`;
+
+export const createSermonMutation = gql`
+  mutation createSermonMutation($objects: [sermon_insert_input!]!) {
+    insert_sermon(objects: $objects) {
+      returning {
+        ...sermonFragment
+      }
+    }
+  }
+  ${sermonFragment}
+`;
+
+export const updateSermonMutation = gql`
+  mutation updateSermonMutation($id: uuid!, $sermon: sermon_set_input) {
+    update_sermon(where: {id: {_eq: $id}}, _set: $sermon) {
+      returning {
+        ...sermonFragment
+      }
+    }
+  }
+  ${sermonFragment}
+`;
+
+export const deleteSermonMutation = gql`
+  mutation deleteSermonMutation($id: uuid!) {
+    delete_sermon(where: {id: {_eq: $id}}) {
+      returning {
+        ...sermonFragment
+      }
+    }
+  }
+  ${sermonFragment}
+`;
