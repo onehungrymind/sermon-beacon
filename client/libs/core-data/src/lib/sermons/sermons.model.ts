e1d4a41852211cdacf637b4cbcaa21c4ccae9abb
davidavila2 <38518209+davidavila2@users.noreply.github.com>
2019-12-05 08:32:16 -0700

    Create: sb-18-create sermon service. (#27)
    * created service for sermons. Now to test it works
    
    * got the apollo to work with graphql. now to clean up template.
    
    * removed test code ready for PR
    
    * Update core-data.module.ts

---
+++ client/libs/core-data/src/lib/sermons/sermons.model.ts
@@ -0,0 +1,19 @@
+export interface Sermon {
+  id: string;
+  title: string;
+  subject?: string;
+  date?: string;
+  created_at: string;
+  updated_at: string;
+  media_id: number;
+}
+
+export const emptySermon: Sermon = {
+  id: null,
+  title: '',
+  subject: '',
+  date: '',
+  created_at: '',
+  updated_at: '',
+  media_id: null
+}
