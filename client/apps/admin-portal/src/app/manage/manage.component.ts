4e82b1f4f7cb84f130c6157a5b7517d736312c66
david <david@venturplex.com>
2019-12-09 14:14:41 -0700

    ready for PR

--- client/apps/admin-portal/src/app/manage/manage.component.ts
+++ client/apps/admin-portal/src/app/manage/manage.component.ts
@@ -9,60 +9,8 @@ import { Tags, TagsService, emptyTags } from '@sb/core-data';
 
 export class ManageComponent implements OnInit {
 
-  primaryColor = 'red';
-  tags$;
-  selectedTags: Tags;
+  constructor() { }
 
-  constructor(private tagsService: TagsService) { }
-
-  ngOnInit() {
-    this.getTags();
-    this.resetTags();
-  }
-
-  selectTags(tags) {
-    this.selectedTags = tags;
-  }
-
-  resetTags() {
-    this.selectTags(emptyTags);
-  }
-
-  getTags() {
-    this.tags$ = this.tagsService.all();
-  }
-
-  saveTags(tags: Tags) {
-    if (!tags.id) {
-      this.createTags(tags);
-    } else {
-      this.updateTags(tags);
-    }
-  }
-
-  createTags(tags: Partial<Tags>) {
-    this.tagsService.create({ ...tags })
-      .subscribe(() => {
-        this.getTags();
-        this.resetTags();
-      });
-  }
-
-  updateTags(tags: Tags) {
-    this.tagsService.update(tags)
-      .subscribe(() => {
-        this.getTags();
-        this.resetTags();
-      });
-  }
-
-  deleteTags(tags: Tags) {
-    this.tagsService.delete(tags)
-      .subscribe(() => this.getTags());
-  }
-
-  cancel() {
-    this.resetTags();
-  }
-
+  ngOnInit() { }
+  
 }
