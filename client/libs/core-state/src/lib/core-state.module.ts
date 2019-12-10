b28ce1a0deac8fe4de662b5dd05410f1a4e8dd26
Austin Johnson <shival.bigman@gmail.com>
2019-12-09 19:03:21 -0700

    removed unneeded ngrx scaffolding

--- client/libs/core-state/src/lib/core-state.module.ts
+++ client/libs/core-state/src/lib/core-state.module.ts
@@ -7,12 +7,6 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { SermonsEffects } from './sermons/sermons.effects';
 import { SermonsFacade } from './sermons/sermons.facade';
-import { SpeakersEffects } from './speakers/speakers.effects';
-import { SpeakersFacade } from './speakers/speakers.facade';
-import { TagsEffects } from './tags/tags.effects';
-import { TagsFacade } from './tags/tags.facade';
-import { MediaEffects } from './media/media.effects';
-import { MediaFacade } from './media/media.facade';
 import { reducers } from '.';
 
 @NgModule({
@@ -22,17 +16,11 @@ import { reducers } from '.';
     StoreModule.forRoot(reducers),
     EffectsModule.forRoot([
       SermonsEffects,
-      SpeakersEffects,
-      TagsEffects,
-      MediaEffects
     ]),
     StoreDevtoolsModule.instrument(),
   ],
   providers: [
     SermonsFacade,
-    SpeakersFacade,
-    TagsFacade,
-    MediaFacade
   ]
 })
 export class CoreStateModule {}
