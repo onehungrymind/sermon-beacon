89956257139f1fc5a66c557a3aa9bb3939cad7a2
Austin Johnson <shival.bigman@gmail.com>
2019-12-10 12:47:49 -0700

    added missing import to index file in core-data (#43)

--- client/libs/core-data/src/lib/media/media.service.ts
+++ client/libs/core-data/src/lib/media/media.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
-import { Media } from './media';
+import { Media } from './media.model';
 import { mediaQuery, createMediaMutation, updateMediaMutation, deleteMediaMutation } from './media.graphql';
 import { map } from 'rxjs/operators';
 import { ApolloQueryResult } from 'apollo-client';
