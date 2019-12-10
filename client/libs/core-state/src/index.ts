2c215484c82a0a874238c721b67d78d1fc89613b
Austin Johnson <shival.bigman@gmail.com>
2019-12-10 10:22:05 -0700

    minor formatting

--- client/libs/core-state/src/index.ts
+++ client/libs/core-state/src/index.ts
@@ -1,4 +1,4 @@
+export { CoreStateModule } from './lib/core-state.module';
+
 export { SermonsFacade } from './lib/sermons/sermons.facade';
 export { SpeakersFacade } from './lib/speakers/speakers.facade';
-
-export { CoreStateModule } from './lib/core-state.module';
