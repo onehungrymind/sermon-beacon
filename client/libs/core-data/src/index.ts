2b520438106dee63bc7c528bdb8cb000f3768110
davidavila2 <38518209+davidavila2@users.noreply.github.com>
2019-12-10 19:57:57 -0700

    SB-32: Media Type Service (#46)
    * created media types service
    
    * made changes to fix network errors

--- client/libs/core-data/src/index.ts
+++ client/libs/core-data/src/index.ts
@@ -11,5 +11,6 @@ export { DialogService } from './lib/shared/dialog/dialog.service';
 export { NotifyService } from './lib/shared/notify/notify.service';
 export { SermonsService } from './lib/sermons/sermons.service';
 export { MediaService } from './lib/media/media.service';
+export { MediaTypesService } from './lib/media-types/media-types.service';
 export { SpeakersService } from './lib/speakers/speakers.service';
 export { TagsService } from './lib/tags/tags.service';
