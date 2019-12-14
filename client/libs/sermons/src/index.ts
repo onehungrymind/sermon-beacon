993be069345def3147a8be28eaa14ab4f5648083
Victor Avila <victor.avila@venturplex.com>
2019-12-13 22:11:03 -0700

    fix: 🐛 remove sermonlint component from index.ts

--- client/libs/sermons/src/index.ts
+++ client/libs/sermons/src/index.ts
@@ -1,4 +1,3 @@
 export { SermonsModule } from './lib/sermons.module';
 
 export { SermonsComponent } from './lib/sermons/sermons.component';
-export { SermonListComponent } from './lib/sermons/sermon-list/sermon-list.component';
