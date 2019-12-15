ffe8b9b93cc5429d062c50759a874b8de962768c
Victor Avila <victor.avila@venturplex.com>
2019-12-14 22:13:00 -0700

    style: 💄 update delete dialog styling

--- client/libs/ui-libraries/src/lib/ui-services/dialog/dialog.service.ts
+++ client/libs/ui-libraries/src/lib/ui-services/dialog/dialog.service.ts
@@ -12,13 +12,14 @@ export class DialogService {
 
   deleteDialog(feature: any, type: string) {
     const ref = this.dialog.open(UiModalComponent);
-    ref.componentInstance.type = type;
+    const { componentInstance } = ref;
 
-    if (feature as Media) ref.componentInstance.media = { ...feature };
-    if (feature as MediaType) ref.componentInstance.mediaType = { ...feature };
-    if (feature as Sermon) ref.componentInstance.sermon = { ...feature };
-    if (feature as Speaker) ref.componentInstance.speaker = { ...feature };
-    if (feature as Tag) ref.componentInstance.tag = { ...feature };
+    componentInstance.type = type;
+    if (feature as Media) componentInstance.media = { ...feature };
+    if (feature as MediaType) componentInstance.mediaType = { ...feature };
+    if (feature as Sermon) componentInstance.sermon = { ...feature };
+    if (feature as Speaker) componentInstance.speaker = { ...feature };
+    if (feature as Tag) componentInstance.tag = { ...feature };
 
     return ref.afterClosed();
   }
