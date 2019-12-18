5c5e5086af32ec0c772a6ac4c51a60a3620151c0
Victor Avila <victor.avila@venturplex.com>
2019-12-17 18:06:48 -0700

    feat: 🎸 updates to tags

--- client/libs/sermons/src/lib/sermons-dialog/sermons-tags/sermons-tags.component.ts
+++ client/libs/sermons/src/lib/sermons-dialog/sermons-tags/sermons-tags.component.ts
@@ -1,11 +1,11 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MatAutocomplete, MatAutocompleteSelectedEvent, MatChipInputEvent } from '@angular/material';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 import * as _ from 'lodash';
-import { Observable } from 'rxjs';
-import { map, startWith, tap } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil, tap } from 'rxjs/operators';
 
 import { Tag } from '@sb/core-data';
 import { TagsFacade } from '@sb/core-state';
@@ -15,14 +15,15 @@ import { TagsFacade } from '@sb/core-state';
   templateUrl: './sermons-tags.component.html',
   styleUrls: ['./sermons-tags.component.scss']
 })
-export class SermonsTagsComponent implements OnInit {
+export class SermonsTagsComponent implements OnInit, OnDestroy {
   @Input() tagsGroup: FormGroup;
   @ViewChild('tagsInput', {static: false}) tagsInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
-  allTags: Tag[];
-  selectedTags: Tag[] = [];
-  filteredTags: Observable<Tag[]>;
-  tags$: Observable<Tag[]> = this.tagsFacade.allTags$;
+  allTags: Tag[]; // All tags after they have been unwrapped from its Observable.
+  selectedTags: Tag[] = []; // Tags currently in the input ("selected").
+  filteredTags: Observable<Tag[]>; // eventually will be on "filtered out" tags, without "selected" ones.
+  tags$: Observable<Tag[]> = this.tagsFacade.allTags$; // All tags from API.
+  destroy$ = new Subject();
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
   constructor(private tagsFacade: TagsFacade) { }
@@ -30,15 +31,21 @@ export class SermonsTagsComponent implements OnInit {
   ngOnInit() {
     this.tagsFacade.loadTags();
     this.tags$.pipe(
-      tap((tags: Tag[]) => this.allTags = tags)
-      ).subscribe();
+      tap((tags: Tag[]) => this.allTags = tags),
+      takeUntil(this.destroy$)
+    ).subscribe();
     this.filteredTags = this.tagsGroup.get('value').valueChanges.pipe(
-      map((tag: Tag) => tag ? this.filterSelected(tag) : this.allTags.slice())
+      map((tagValue: string) => this.filterSelected(tagValue))
     );
   }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
+
   add(event: MatChipInputEvent) {
-    // Add fruit only when MatAutocomplete is not open
+    // Add Tag only when MatAutocomplete is not open
     // To make sure this does not conflict with OptionSelected Event
     if (!this.matAutocomplete.isOpen) {
       const input = event.input;
@@ -72,8 +79,9 @@ export class SermonsTagsComponent implements OnInit {
     this.tagsGroup.get('value').setValue(null);
   }
 
-  private filterSelected(tag: Tag) {
-    return _.filter(this.allTags, tag);
-  }
+  private filterSelected(tagValue: string) {
+      const filterValue = tagValue.toLowerCase();
 
+      return this.allTags.filter((tag: Tag) => tag.value.toLowerCase().indexOf(filterValue) === 0);
+    }
 }
