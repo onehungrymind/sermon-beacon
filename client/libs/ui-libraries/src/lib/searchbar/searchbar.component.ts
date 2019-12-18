79d15ae90111cc7db5c3b1fd0898e597f3ee2323
Victor Avila <victor.avila@venturplex.com>
2019-12-18 16:57:26 -0700

    style: 💄 styling updates for searchbar

--- client/libs/ui-libraries/src/lib/searchbar/searchbar.component.ts
+++ client/libs/ui-libraries/src/lib/searchbar/searchbar.component.ts
@@ -1,31 +1,36 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { MatDatepicker } from '@angular/material';
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+
+import * as moment from 'moment';
+import { map, takeUntil, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'sb-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+  styleUrls: ['./searchbar.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class SearchbarComponent implements OnInit {
+export class SearchbarComponent implements OnDestroy, OnInit {
+  @ViewChild('picker', { static: false }) datePicker: MatDatepicker<any>;
   form: FormGroup;
-  webView: boolean;
+  destroy$ = new Subject;
   categoryOptions = [
     { name: 'Sermon Title' },
     { name: 'Sermon Speaker' },
     { name: 'Sermon Date', icon: 'calendar_today' }
   ];
-  @ViewChild('picker', { static: false }) datePicker: MatDatepicker<any>;
 
-  constructor (
-    private fb: FormBuilder,
-    public breakpointObserver: BreakpointObserver
-  ) { }
+  constructor (private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.initForm();
-    this.initMobileView();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
   search(formValue) {
@@ -34,10 +39,16 @@ export class SearchbarComponent implements OnInit {
 
   selectCustom(searchType: string, index = 0) {
     const datepickerOption = 2;
+
     this.form.patchValue({ searchType: searchType });
 
     if (index === datepickerOption) {
       this.datePicker.open();
+      this.datePicker.closedStream.pipe(
+        map(() => moment(this.datePicker._selected).format('MM/DD/YYYY')),
+        tap((formattedDate: string) => this.form.get('searchQuery').patchValue(formattedDate)),
+        takeUntil(this.destroy$)
+      ).subscribe();
     }
   }
 
@@ -46,21 +57,9 @@ export class SearchbarComponent implements OnInit {
     this.form.patchValue({ searchType: 'Advanced' });
   }
 
-  private initMobileView() {
-    this.breakpointObserver
-      .observe(['(min-width: 650px)'])
-      .subscribe((state: BreakpointState) => {
-        if (state.matches) {
-          this.webView = true;
-        } else {
-          this.webView = false;
-        }
-      });
-  }
-
-  private initForm(): void {
-    this.form = this.fb.group({
-      search: [''],
+  private initForm() {
+    this.form = this.formBuilder.group({
+      searchQuery: [''],
       searchType: ['Advanced']
     });
   }
