11048751d9d404ae0dec00b732c5ff7091b35323
Victor Avila <victor.avila@venturplex.com>
2019-12-23 00:02:09 -0700

    feat: 🎸 first pass sermon form patching

--- client/libs/sermons/src/lib/sermons-dialog/sermons-dialog.component.ts
+++ client/libs/sermons/src/lib/sermons-dialog/sermons-dialog.component.ts
@@ -2,6 +2,10 @@ import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatTabGroup } from '@angular/material';
 
+import { combineLatest } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { MediaFacade, SermonsFacade, TagsFacade } from '@sb/core-state';
 import { Sermon } from '@sb/core-data';
 
 @Component({
@@ -11,15 +15,28 @@ import { Sermon } from '@sb/core-data';
 })
 export class SermonsDialogComponent implements OnInit {
   form: FormGroup;
+  sermon$ = this.sermonFacade.selectedSermon$;
+  sermonMedia$ = this.mediaFacade.allMedia$;
+  sermonTags$ = this.tagsFacade.allTags$;
   @ViewChild(MatTabGroup, { static: true }) tabGroup: MatTabGroup;
 
   constructor(
     private formBuilder: FormBuilder,
+    private sermonFacade: SermonsFacade,
+    private mediaFacade: MediaFacade,
+    private tagsFacade: TagsFacade,
     @Inject(MAT_DIALOG_DATA) public data: Sermon
   ) {}
 
   ngOnInit() {
+    const { id } = this.data;
     this.initForm();
+    if (this.data && id) {
+      this.sermonFacade.selectSermon(id);
+      this.mediaFacade.loadMediaBySermonId(id);
+      this.tagsFacade.loadTagsBySermonId(id);
+      this.selectSermon$().subscribe();
+    }
   }
 
   next() {
@@ -33,6 +50,22 @@ export class SermonsDialogComponent implements OnInit {
   addMediaGroup() {
     const media = this.form.get('media') as FormArray;
     media.push(this.mediaGroup());
+  }
+
+  selectSermon$() {
+    return combineLatest([
+      this.sermon$,
+      this.sermonMedia$,
+      this.sermonTags$
+    ]).pipe(
+      tap(([sermon, media, tags]) => {
+        this.form.patchValue({
+          details: sermon,
+          media,
+          tags: { tags }
+        });
+      })
+    );
   }
 
   private initForm() {
